6b4c530f153b9969b770be16cf577a5d4d88ffc0
Dustin Lo <dustinlo@users-MacBook-Pro-2.local>
2019-01-29 23:51:17 -0800

    added node append to Node.java

--- src/com/example/practice/LinkedListPractice.java
+++ src/com/example/practice/LinkedListPractice.java
@@ -57,5 +57,25 @@ public class LinkedListPractice {
         list2.printLinkedList();
         list2.reverseLinkedListPerm();
         list2.printLinkedList();
+
+
+        System.out.println();
+        Node n1 = new Node(1);
+        Node n2 = new Node(2); n1.appentTail(n2);
+        Node n3 = new Node(3); n2.appentTail(n3);
+        Node n4 = new Node(4); n3.appentTail(n4);
+        Node n5 = new Node(5); n4.appentTail(n5);
+        Node n6 = new Node(6); n5.appentTail(n6);
+        Node n7 = new Node(7); n6.appentTail(n7);
+
+        Node a1 = new Node(10);
+        Node a2 = new Node(11); a1.appentTail(a2);
+        Node a3 = new Node(12); a2.appentTail(a3);
+        a3.appentTail(n5);
+
+        System.out.println("1 ->  2 ->  3 ->  4 ->  5 ->  6 -> 7\n" +
+                "                        ^\n" +
+                "                        |\n" +
+                "      10-> 11->  12 ----|");
     }
 }
--- src/com/example/practice/Node.java
+++ src/com/example/practice/Node.java
@@ -7,4 +7,23 @@ public class Node {
     public Node(int d) {
         data = d;
     }
+
+    public void appentTail(Node newNode) {
+        this.next = newNode;
+        return;
+    }
+
+    public void printNodes() {
+        Node cur = this;
+        while(cur != null) {
+            if(cur.next != null) {
+                String str = String.format("%2d -> ", cur.data);
+                System.out.print(str);
+            } else {
+                System.out.print(cur.data);
+            }
+            cur = cur.next;
+        }
+        System.out.println();
+    }
 }
