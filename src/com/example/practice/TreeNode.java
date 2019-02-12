7e335efd643d330359fe60ad6631b16ce6b81ab3
Dustin Lo <dustinlo@users-mbp-2.lan>
2019-02-11 22:20:44 -0800

    minimum height tree working

--- src/com/example/practice/TreeNode.java
+++ src/com/example/practice/TreeNode.java
@@ -2,6 +2,7 @@ package com.example.practice;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class TreeNode {
     public int data;
@@ -161,7 +162,7 @@ public class TreeNode {
         root.insertInOrder(37);
         listOfDepths(root);
 
-        int[] nodeArr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
+        int[] nodeArr = IntStream.range(1, 102).toArray();
         TreeNode minHeightTree = minHeightTree(nodeArr);
         listOfDepths(minHeightTree);
     }
